c34a08a34c9ce62ef369aeb9287c16fc3a3cd7fc
diego62599 <goofy62599@gmail.com>
2023-05-12 08:40:52 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require ('express')
+const path = require ('path')
+const app = express ()
+const hbs = require ('hbs')
+const puerto = 1010
+app.use (express.static('public'))
+
+app.set ('views', path.join(__dirname+'/public/views'))
+app.set ('view engine', 'hbs')
+hbs.registerPartials(__dirname+ '/public/views/partials');
+
+
+app.get('/', (req , res) => {
+    res.write ('Home',
+    {
+      titulo : 'Home',
+      nombre:'Diego Betancur'  
+    }) 
+})
+
+app.listen (puerto, () => {
+    console.log (`Escuchando el puerto ${puerto}`)
+
+} )
+
